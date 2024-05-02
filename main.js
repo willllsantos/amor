bcb950daa93324e647efc9afcb0be9801992110c
willian <williansantos16@outlook.com.br>
2024-05-01 22:34:27 -0300

    atualizaçao

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ function clicar() {
     var s = document.getElementById("sim")
     s.innerText = "SIM"    
     s.style.backgroundColor = "green"
+    
 }
+
+setTimeout(function() {
+    location.reload();
+}, 2000)
 
 
