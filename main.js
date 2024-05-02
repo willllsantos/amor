2186d3ee48c9ca4adf88d932cb9421cdc477f868
willian <williansantos16@outlook.com.br>
2024-05-02 16:35:53 -0300

    Descrição das modificações

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 function clicar() {
     var s = document.getElementById("sim")
-    s.innerText = "SIM"    
+    s.innerText = "SIM, EU VOU TE DAR MEU AMOR =) "    
     s.style.backgroundColor = "green"
+
+    setTimeout(function() {
+        location.reload();
+    }, 2000)
     
 }
 
-setTimeout(function() {
-    location.reload();
-}, 2000)
+function clicou() {
+    var c = document.getElementById("nao")
+    c.innerText = "SIM, EU VOU TE DAR MEU AMOR :} "    
+    c.style.backgroundColor = "green"
+
+    setTimeout(function() {
+        location.reload();
+    }, 2000)
+}
 
 
